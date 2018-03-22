734eb92bbc147cafb2b7966c7b81afaf8ee45a77
Vikash Chahal <vchahal2@sapient.com>
2018-03-23 02:18:24 +0530

    Adding Pub/Sub Sender and Consumer Changes also

--- src/main/java/com/example/demo/SbWebTestApplication.java
+++ src/main/java/com/example/demo/SbWebTestApplication.java
@@ -1,22 +1,76 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
+import org.springframework.cloud.gcp.pubsub.integration.AckMode;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
+import org.springframework.cloud.gcp.pubsub.support.GcpHeaders;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.web.client.RestTemplate;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
 
 @SpringBootApplication
 public class SbWebTestApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SbWebTestApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SbWebTestApplication.class, args);
-	}	  
-	  
+	}
+
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
+
+	@Bean
+	public MessageChannel pubsubInputChannel() {
+		return new DirectChannel();
+	}
+
+	@Bean
+	public PubSubInboundChannelAdapter messageChannelAdapter(
+			@Qualifier("pubsubInputChannel") MessageChannel inputChannel, PubSubOperations pubSubTemplate) {
+		LOGGER.info("We are inside messageChannelAdapter method ");
+		PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter(pubSubTemplate, "testSubscription");
+		adapter.setOutputChannel(inputChannel);
+		adapter.setAckMode(AckMode.MANUAL);
+
+		return adapter;
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "pubsubInputChannel")
+	public MessageHandler messageReceiver() {
+		return message -> {
+			LOGGER.info("Message arrived! Payload: " + message.getPayload());
+			AckReplyConsumer consumer = (AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
+			consumer.ack();
+		};
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "pubsubOutputChannel")
+	public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+		LOGGER.info("inside message sender");
+		return new PubSubMessageHandler(pubsubTemplate, "testTopic");
+	}
+
+	@MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+	public interface PubsubOutboundGateway {
+		void sendToPubsub(String text);
+	}
+
 }
--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -4,17 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
+import com.example.demo.SbWebTestApplication.PubsubOutboundGateway;
 import com.example.demo.pojo.UnprocessedText;
 import com.example.demo.service.SbService;
+
 
 @RestController
 public class ApplicationController {
@@ -24,9 +27,18 @@ public class ApplicationController {
 	@Autowired
 	SbService sbService;
 
+	@Autowired
+	private PubsubOutboundGateway messagingGateway;
+
 	@GetMapping("/")
 	public String hello() {
 		return "hello world from new web application!";
+	}
+
+	@PostMapping("/publishMessage")
+	public ResponseEntity<String> publishMessage(@RequestParam("message") String message) {
+		messagingGateway.sendToPubsub(message);
+		return new ResponseEntity<String>("Message Published and Consumed", HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/processMessage", method = RequestMethod.POST)
